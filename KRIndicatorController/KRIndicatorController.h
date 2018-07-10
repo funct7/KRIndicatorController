b8b4dc6375fc27dc3d91c93c0981a856a281f7e1
Joshua Park <wmpark@knowre.com>
2018-07-11 08:18:54 +0900

    Initial Commit

---
+++ KRIndicatorController/KRIndicatorController.h
@@ -0,0 +1,19 @@
+//
+//  KRIndicatorController.h
+//  KRIndicatorController
+//
+//  Created by Joshua Park on 11/07/2018.
+//  Copyright © 2018 Knowre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KRIndicatorController.
+FOUNDATION_EXPORT double KRIndicatorControllerVersionNumber;
+
+//! Project version string for KRIndicatorController.
+FOUNDATION_EXPORT const unsigned char KRIndicatorControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KRIndicatorController/PublicHeader.h>
+
+
